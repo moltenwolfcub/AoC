dcc027b97a453b2d69efb3e6f7395c22a2855b7c
MoltenWolfCub <oliver@tomlins.org.uk>
2022-12-01 19:23:00 +0000

    refactored Day1_1

--- 2022/Day1_1.java
+++ 2022/Day1_1.java
@@ -1,62 +1,23 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
+import Helpers.FileUtils;
 
 public class Day1_1 {
     private static Integer mostCalories = 0;
-    private static List<List<String>> elfSeperatedCalories = new ArrayList<>();
-    private static List<Integer> totalElfCalories = new ArrayList<>();
-
-    //Retrieve the file
-    private static List<String> allElfCalories = readFile("2022/day1_1.txt");
+    private static List<String> elfCalorieData;
 
     public static void main(String[] args) {
-        //seperate out the individual elves by the space delimiter.
-        List<String> tmpList = new ArrayList<>();
-        for (String calorieData : allElfCalories) {
-            if (calorieData.isEmpty()) {
-                elfSeperatedCalories.add(tmpList);
-                tmpList = new ArrayList<>();
+        elfCalorieData = FileUtils.readLines("2022/day1_1.txt");
+
+        Integer currentTotal = 0;
+        for (String calorie : elfCalorieData) {
+            if (calorie.isBlank()) {
+                mostCalories = Math.max(currentTotal, mostCalories);
+                currentTotal = 0;
             } else {
-                tmpList.add(calorieData);
+                currentTotal += Integer.valueOf(calorie);
             }
         }
-        elfSeperatedCalories.add(tmpList);
-
-        //sum each elf's individual food items.
-        for (List<String> individualElfCalories : elfSeperatedCalories) {
-            Integer calorieSum = 0;
-            for (String calorie : individualElfCalories) {
-                calorieSum += Integer.valueOf(calorie);
-            }
-            totalElfCalories.add(calorieSum);
-        }
-
-        //find the biggest item in the list.
-        totalElfCalories.forEach((elfCalorieCount) -> {
-            mostCalories = Math.max(mostCalories, elfCalorieCount);
-        });
-
         System.out.println(mostCalories);
-    }
-
-    private static List<String> readFile(String fileName) {
-        List<String> data = new ArrayList<>();
-
-        try {
-            File file = new File(fileName);
-            Scanner fileReader = new Scanner(file);
-            while (fileReader.hasNextLine()) {
-                data.add(fileReader.nextLine());
-            }
-            fileReader.close();
-        } catch (IOException e) {
-            System.out.println("An Error Occured while reading in the file.");
-            e.printStackTrace();
-            System.out.println("The data being returned will be incomplete or incorrect.");
-        }
-        return data;
     }
 }
---
+++ Helpers/FileUtils.java
@@ -0,0 +1,30 @@
+package Helpers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileUtils {
+    public static List<String> readLines(String fileName) {
+        List<String> lines = new ArrayList<>();
+
+        try {
+            File file = new File(fileName);
+
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                lines.add(fileReader.nextLine());
+            }
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("An Error Occured while reading in the file.");
+            e.printStackTrace();
+            System.out.println("The data being returned will be incomplete or incorrect.");
+        }
+        
+        return lines;
+    }
+}
