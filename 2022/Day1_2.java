6405dab46b0bdc437865f7333411ca7f1358ffeb
MoltenWolfCub <oliver@tomlins.org.uk>
2022-12-01 19:55:32 +0000

    refactored Day1_2

--- 2022/Day1_2.java
+++ 2022/Day1_2.java
@@ -7,44 +7,29 @@ import Helpers.FileUtils;
 
 public class Day1_2 {
     private static List<String> elfCalorieData;
-    private static List<Integer> topCalorieCounts;
+    private static List<Integer> allCalorieSums;
     private static Integer topCombinedCaloriesSum;
 
     public static void main(String[] args) {
         elfCalorieData = FileUtils.readLines("2022/day1.txt");
-        topCalorieCounts = new ArrayList<Integer>(Arrays.asList(0, 0, 0));
+        allCalorieSums = new ArrayList<Integer>(Arrays.asList(0, 0, 0));
         topCombinedCaloriesSum = 0;
 
         Integer currentTotal = 0;
         for (String calorie : elfCalorieData) {
             if (calorie.isBlank()) {
-
-                //add new item
-                topCalorieCounts.add(currentTotal);
-
-                //put in descending order
-                Collections.sort(topCalorieCounts);
-                Collections.reverse(topCalorieCounts);
-
-                //make a new list of the top 3
-                List<Integer> newList = new ArrayList<>();
-                for (int i = 0; i < 3; i++) {
-                    newList.add(topCalorieCounts.get(i));
-                }
-                //replace the original list with the top 3 list
-                topCalorieCounts.clear();
-                topCalorieCounts.addAll(newList);
-
+                allCalorieSums.add(currentTotal);
                 currentTotal = 0;
 
             } else {
                 currentTotal += Integer.valueOf(calorie);
             }
         }
-        
-        //sum the calories
-        for (Integer calorie : topCalorieCounts) {
-            topCombinedCaloriesSum += calorie;
+
+        Collections.sort(allCalorieSums);
+        Collections.reverse(allCalorieSums);
+        for (int i = 0; i < 3; i++) {
+            topCombinedCaloriesSum += allCalorieSums.get(i);
         }
         System.out.println(topCombinedCaloriesSum);
     }
