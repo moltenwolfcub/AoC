dfbf9db6e224e2e9fee3c142185c84ce1cde734c
MoltenWolfCub <oliver@tomlins.org.uk>
2022-12-04 14:48:39 +0000

    4th December 2022 part 1

---
+++ 2022/day4_1.java
@@ -0,0 +1,31 @@
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+import Helpers.FileUtils;
+
+public class day4_1 {
+    private static Integer runningTotal;
+
+    public static void main(String[] args) {
+        runningTotal = 0;
+
+        for (String line : FileUtils.readLines("2022/day4.txt")) {
+            String[] elves = line.split(",");
+
+            Supplier<IntStream> firstAssignedSegs = ()-> IntStream.range(Integer.valueOf(elves[0].split("-")[0]), Integer.valueOf(elves[0].split("-")[1])+1);
+            Supplier<IntStream> secondAssignedSegs = ()-> IntStream.range(Integer.valueOf(elves[1].split("-")[0]), Integer.valueOf(elves[1].split("-")[1])+1);
+
+            Long firstCount = firstAssignedSegs.get().count();
+            Long secondCount = secondAssignedSegs.get().count();
+            Boolean isFirstBigger = firstCount > secondCount;
+
+            Supplier<IntStream> longer = isFirstBigger ? firstAssignedSegs : secondAssignedSegs;
+            Supplier<IntStream> shorter = isFirstBigger ? secondAssignedSegs : firstAssignedSegs;
+
+            if (shorter.get().allMatch(shortSeg-> longer.get().anyMatch(longSeg-> longSeg == shortSeg))) {
+                runningTotal++;
+            }
+        }
+        System.out.println(runningTotal);
+    }
+}
