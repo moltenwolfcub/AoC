9a94a6016e1d6146c519995d34aa512c67b29cc0
MoltenWolfCub <oliver@tomlins.org.uk>
2022-12-18 15:02:47 +0000

    2022-14-1

---
+++ Helpers/Axis.java
@@ -0,0 +1,40 @@
+package Helpers;
+
+public enum Axis {
+    X,
+    Y;
+
+    private Axis() {
+
+    }
+
+    public Position2D movePosition(Position2D originalPos, Integer delta) {
+        if(this.equals(Axis.X)) {
+            return new Position2D(originalPos.x+delta, originalPos.y);
+        } else if(this.equals(Axis.Y)) {
+            return new Position2D(originalPos.x, originalPos.y+delta);
+        } else {
+            throw new IllegalArgumentException("Can't move a 2d position by any axis except X or Y.");
+        }
+    }
+
+    public Integer getPositionAxisValue(Position2D pos) {
+        if(this.equals(Axis.X)) {
+            return pos.x;
+        } else if(this.equals(Axis.Y)) {
+            return pos.y;
+        } else {
+            throw new IllegalArgumentException("Can't get any axis except X or Y from a 2d position.");
+        }
+    }
+
+    public Axis getOtherAxis2D() {
+        if(this.equals(Axis.X)) {
+            return Axis.Y;
+        } else if(this.equals(Axis.Y)) {
+            return Axis.X;
+        } else {
+            throw new IllegalArgumentException("Current Axis isn't a 2D Axis.");
+        }
+    }
+}
--- Helpers/Position2D.java
+++ Helpers/Position2D.java
@@ -43,7 +43,7 @@ public class Position2D implements Cloneable {
         }
         if (other instanceof Position2D) {
             Position2D pos = (Position2D) other;
-            return pos.x == this.x && pos.y == this.y;
+            return pos.x.equals(this.x) && pos.y.equals(this.y);
         }
         return false;
     }
