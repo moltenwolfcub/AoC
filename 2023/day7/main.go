2b3a3c0d5bb45083b3fcf09c36b2ccfbc0c53842
MoltenWolfCub <oliver@tomlins.org.uk>
2023-12-09 09:01:32 +0000

    2023-7-2

--- 2023/day7/main.go
+++ 2023/day7/main.go
@@ -12,6 +12,7 @@ func main() {
 	input := helpers.ReadLines("input.txt")
 
 	hands := []hand{}
+	hands2 := []hand{}
 
 	for _, line := range input {
 		if line == "" {
@@ -20,12 +21,18 @@ func main() {
 
 		inputParts := strings.Split(line, " ")
 		rawHand := inputParts[0]
-		handType := calcHand(rawHand)
+		handType := calcHand(rawHand, false)
+		handType2 := calcHand(rawHand, true)
 
 		bid := helpers.MustAtoi(inputParts[1])
 
 		hands = append(hands, hand{
 			hType:  handType,
+			bid:    bid,
+			rawVal: rawHand,
+		})
+		hands2 = append(hands2, hand{
+			hType:  handType2,
 			bid:    bid,
 			rawVal: rawHand,
 		})
@@ -62,10 +69,46 @@ func main() {
 	for i, h := range hands {
 		total += (i + 1) * h.bid
 	}
-	fmt.Println(total)
+	fmt.Printf("Part 1: %d\n", total)
+
+	//Part 2
+
+	//sort from highest-lowest
+	sort.Slice(hands2, func(i, j int) bool {
+		iHand := hands2[i]
+		jHand := hands2[j]
+
+		if iHand.hType < jHand.hType {
+			return false
+		}
+		if iHand.hType > jHand.hType {
+			return true
+		}
+
+		for i := 0; i < len(iHand.rawVal); i++ {
+			ci := rune(iHand.rawVal[i])
+			cj := rune(jHand.rawVal[i])
+			comp := isStronger2(ci, cj)
+
+			if comp < 0 {
+				return false
+			}
+			if comp > 0 {
+				return true
+			}
+		}
+		panic("Same string")
+	})
+
+	total = 0
+	for i, h := range hands2 {
+		total += (i + 1) * h.bid
+	}
+	fmt.Printf("Part 2: %d\n", total)
 }
 
 var runeStrength string = "AKQJT98765432"
+var runeStrength2 string = "AKQT98765432J"
 
 // is i stronger than j
 func isStronger(i, j rune) int {
@@ -75,10 +118,24 @@ func isStronger(i, j rune) int {
 	return iIndex - jIndex
 }
 
-func calcHand(hand string) handType {
+// is i stronger than j
+func isStronger2(i, j rune) int {
+	iIndex := strings.IndexRune(runeStrength2, i)
+	jIndex := strings.IndexRune(runeStrength2, j)
+
+	return iIndex - jIndex
+}
+
+func calcHand(hand string, jokers bool) handType {
 	handData := map[rune]int{}
+	jokerCount := 0
 
 	for _, c := range hand {
+		if c == 'J' && jokers {
+			jokerCount++
+			continue
+		}
+
 		amount, ok := handData[c]
 		if !ok {
 			handData[c] = 1
@@ -86,6 +143,17 @@ func calcHand(hand string) handType {
 		}
 		handData[c] = amount + 1
 	}
+
+	maximumNumber := 0
+	maxKey := rune(0)
+	for k, v := range handData {
+		if v > maximumNumber {
+			maxKey = k
+			maximumNumber = v
+		}
+	}
+
+	handData[maxKey] += jokerCount
 
 	found3 := false
 	found2 := false
@@ -117,7 +185,6 @@ func calcHand(hand string) handType {
 	if found2 {
 		return OnePair
 	}
-
 	return HighCard
 }
 
