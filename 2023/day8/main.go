71c320fd2e0aae2b8e973463329af864f90c8506
MoltenWolfCub <oliver@tomlins.org.uk>
2023-12-09 20:03:38 +0000

    2023-8-2

--- 2023/day8/main.go
+++ 2023/day8/main.go
@@ -13,7 +13,16 @@ func main() {
 
 	nodes := buildGraph(input[2:])
 
-	current := nodes["AAA"]
+	fmt.Printf("Part 1: %d\n", part1(nodes, instructions))
+	fmt.Printf("Part 2: %d\n", part2(nodes, instructions))
+}
+
+func part1(nodes map[string]*node, instructions string) int {
+	current, ok := nodes["AAA"]
+	if !ok {
+		fmt.Print("Couldn't find node AAA! ")
+		return 0
+	}
 
 	steps := 0
 
@@ -26,7 +35,41 @@ func main() {
 			break
 		}
 	}
-	fmt.Println(steps)
+	return steps
+}
+
+func part2(nodes map[string]*node, instructions string) int {
+	startingNodes := []*node{}
+
+	for _, n := range nodes {
+		if n.label[2] == 'A' {
+			startingNodes = append(startingNodes, n)
+		}
+	}
+
+	allSteps := []int{}
+
+	for _, n := range startingNodes {
+		current := n
+		steps := 0
+		for {
+			shouldBreak := false
+			for _, c := range instructions {
+				current = current.travel(string(c))
+				steps++
+				if current.label[2] == 'Z' {
+					shouldBreak = true
+					break
+				}
+			}
+			if shouldBreak {
+				break
+			}
+		}
+		allSteps = append(allSteps, steps)
+	}
+
+	return helpers.LCM(allSteps...)
 }
 
 func buildGraph(input []string) map[string]*node {
--- helpers/math.go
+++ helpers/math.go
@@ -15,3 +15,21 @@ func IntMax(a int, b int) int {
 		return b
 	}
 }
+
+func LCM(nums ...int) int {
+	lcm := 1
+	for _, num := range nums {
+		lcm = lcm * num / GCD(lcm, num)
+	}
+	return lcm
+}
+
+func GCD(a, b int) int {
+	t := 0
+	for b != 0 {
+		t = b
+		b = a % b
+		a = t
+	}
+	return a
+}
