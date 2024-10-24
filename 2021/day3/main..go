053a75a8a5c80e5c148dc923c2added91916b000
MoltenWolfCub <oliver@tomlins.org.uk>
2024-10-24 22:05:22 +0100

    2021-3-2

--- 2021/day3/main..go
+++ 2021/day3/main..go
@@ -8,7 +8,11 @@ import (
 
 func main() {
 	input := helpers.ReadLines("input.txt")
+	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
+}
 
+func part1(input []string) int {
 	bits := make([]int, len(input[0]))
 
 	for _, line := range input {
@@ -40,5 +44,72 @@ func main() {
 		epsilon += (1 ^ n) << pos
 	}
 
-	fmt.Println(epsilon * gamma)
+	return epsilon * gamma
+}
+
+func part2(input []string) int {
+	oxygen := input
+	for i := 0; i < len(input[0]); i++ {
+		zeros := []string{}
+		ones := []string{}
+		for _, line := range oxygen {
+			if line == "" {
+				continue
+			}
+			c := helpers.MustAtoi(string(line[i]))
+			if c == 1 {
+				ones = append(ones, line)
+			} else {
+				zeros = append(zeros, line)
+			}
+		}
+		if len(zeros) > len(ones) {
+			oxygen = zeros
+		} else {
+			oxygen = ones
+		}
+
+		if len(oxygen) == 1 {
+			break
+		}
+	}
+
+	co2 := input
+	for i := 0; i < len(input[0]); i++ {
+		zeros := []string{}
+		ones := []string{}
+		for _, line := range co2 {
+			if line == "" {
+				continue
+			}
+			c := helpers.MustAtoi(string(line[i]))
+			if c == 1 {
+				ones = append(ones, line)
+			} else {
+				zeros = append(zeros, line)
+			}
+		}
+		if len(ones) < len(zeros) {
+			co2 = ones
+		} else {
+			co2 = zeros
+		}
+
+		if len(co2) == 1 {
+			break
+		}
+	}
+
+	return binToDec(co2[0]) * binToDec(oxygen[0])
+}
+
+func binToDec(bin string) (output int) {
+	for i, b := range bin {
+		pos := len(bin) - i - 1
+
+		n := helpers.MustAtoi(string(b))
+
+		output += n << pos
+	}
+	return
 }
