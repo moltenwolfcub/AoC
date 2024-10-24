218cab0b1e2c8bd3d878b6636b0d1757fdde4a8a
MoltenWolfCub <oliver@tomlins.org.uk>
2024-10-24 20:36:20 +0100

    2021-2-2

--- 2021/day2/main.go
+++ 2021/day2/main.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 	input := helpers.ReadLines("input.txt")
+	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
+}
 
+func part1(input []string) int {
 	depth := 0
 	hPos := 0
 
@@ -30,5 +34,32 @@ func main() {
 			depth += amount
 		}
 	}
-	fmt.Println(depth * hPos)
+	return depth * hPos
+}
+
+func part2(input []string) int {
+	depth := 0
+	hPos := 0
+
+	aim := 0
+
+	for _, line := range input {
+		if line == "" {
+			continue
+		}
+
+		instruction := strings.Split(line, " ")
+		amount := helpers.MustAtoi(instruction[1])
+
+		switch instruction[0] {
+		case "forward":
+			hPos += amount
+			depth += amount * aim
+		case "up":
+			aim -= amount
+		case "down":
+			aim += amount
+		}
+	}
+	return depth * hPos
 }
