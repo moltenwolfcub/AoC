03d4651e8576a180d47ff7f46c9bc19f85500530
MoltenWolfCub <oliver@tomlins.org.uk>
2024-10-24 20:14:58 +0100

    2021-1-2

--- 2021/day1/main.go
+++ 2021/day1/main.go
@@ -8,7 +8,11 @@ import (
 
 func main() {
 	input := helpers.ReadLines("input.txt")
+	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
+}
 
+func part1(input []string) int {
 	lastValue := -1
 	runningTotal := 0
 
@@ -24,5 +28,43 @@ func main() {
 		}
 		lastValue = num
 	}
-	fmt.Println(runningTotal)
+	return runningTotal
+}
+
+func part2(input []string) int {
+	lastValues := make([]int, 3)
+	runningTotal := 0
+
+	for _, line := range input {
+		if line == "" {
+			continue
+		}
+		num := helpers.MustAtoi(line)
+
+		theseValues := append(lastValues[1:3], num)
+		if listFilled(lastValues) {
+			if sum(theseValues) > sum(lastValues) {
+				runningTotal++
+			}
+		}
+		lastValues = theseValues
+	}
+	return runningTotal
+}
+
+func sum(l []int) (total int) {
+	for _, i := range l {
+		total += i
+	}
+	return
+}
+
+func listFilled(l []int) bool {
+	filled := 0
+	for _, i := range l {
+		if i != 0 {
+			filled++
+		}
+	}
+	return filled >= 3
 }
