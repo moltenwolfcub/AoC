9c8ffd31fbd3f2a28476f39fd4b2a4f8cabc7070
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-03 22:46:38 +0000

    2025-1-2

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -12,7 +12,8 @@ func main() {
 
 	re := regexp.MustCompile(`([LR])(\d+)`)
 
-	zeros := 0
+	part1Zeroes := 0
+	part2Zeroes := 0
 
 	safeIndex := 50
 	for _, line := range input {
@@ -25,6 +26,8 @@ func main() {
 		direction := matches[1]
 		amount := helpers.MustAtoi(matches[2])
 
+		wasZero := safeIndex == 0
+
 		switch direction {
 		case "R":
 			safeIndex += amount
@@ -34,23 +37,39 @@ func main() {
 			panic(fmt.Sprintf("Unknown instruction: '%x'", direction))
 		}
 
-		safeIndex = makePrinciple(safeIndex)
+		originCrosses := 0
+		safeIndex, originCrosses = makePrinciple(safeIndex, wasZero)
+
+		part2Zeroes += originCrosses
 
 		if safeIndex == 0 {
-			zeros++
+			part1Zeroes++
+			part2Zeroes++
 		}
 	}
-	fmt.Println(zeros)
+
+	fmt.Printf("Part 1: %d\n", part1Zeroes)
+	fmt.Printf("Part 2: %d\n", part2Zeroes)
 }
 
-func makePrinciple(rot int) int {
+func makePrinciple(rot int, wasZero bool) (int, int) {
+	originCrosses := 0
+	if wasZero && rot < 0 {
+		originCrosses-- // gone from zero so don't want to double count first move
+	}
+
 	for rot >= 100 || rot < 0 {
 		if rot >= 100 {
+			if rot != 100 {
+				originCrosses++ //didn't cross origin it landed on origin
+			}
 			rot -= 100
 		}
 		if rot < 0 {
 			rot += 100
+			originCrosses++
 		}
 	}
-	return rot
+
+	return rot, originCrosses
 }
