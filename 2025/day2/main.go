556f26c156a524dcff90c709e32a5dc99315b612
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-04 21:22:37 +0000

    2025-2-2
    gotta love regex

--- 2025/day2/main.go
+++ 2025/day2/main.go
@@ -15,28 +15,36 @@ func main() {
 
 	matches := re.FindAllStringSubmatch(input, -1)
 
-	total := 0
+	total1 := 0
+	total2 := 0
 	for _, match := range matches {
 		rangeStart := helpers.MustAtoi(match[1])
 		rangeEnd := helpers.MustAtoi(match[2])
 
 		for i := rangeStart; i <= rangeEnd; i++ {
-			result := testID(i)
-			total += result
+			result1, result2 := testID(i)
+			total1 += result1
+			total2 += result2
 		}
 	}
 
-	fmt.Println(total)
+	fmt.Printf("Part 1: %d\n", total1)
+	fmt.Printf("Part 2: %d\n", total2)
 }
 
 // returns ID if invalid or 0 if valid
-func testID(id int) int {
-	re := pcre.MustCompile(`^(\d+)\1$`, 0) //different regex library to support backreferencing
-
-	hasMatch := re.MatcherString(fmt.Sprint(id), 0).Matches()
-
-	if hasMatch {
-		return id
+func testID(id int) (result1, result2 int) {
+	re1 := pcre.MustCompile(`^(\d+)\1$`, 0) //different regex library to support backreferencing
+	hasMatch1 := re1.MatcherString(fmt.Sprint(id), 0).Matches()
+	if hasMatch1 {
+		result1 = id
 	}
-	return 0
+
+	re2 := pcre.MustCompile(`^(\d+)\1+$`, 0) //different regex library to support backreferencing
+	hasMatch2 := re2.MatcherString(fmt.Sprint(id), 0).Matches()
+	if hasMatch2 {
+		result2 = id
+	}
+
+	return
 }
