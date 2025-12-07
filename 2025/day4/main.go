47bdbb8968c7e11a890b92629c48ee48bdce1543
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-07 21:33:07 +0000

    2024-4-2

--- 2025/day4/main.go
+++ 2025/day4/main.go
@@ -10,12 +10,10 @@ type Vec2D struct {
 	X, Y int
 }
 
-var grid [][]bool
-
 func main() {
 	input := helpers.ReadLines("input.txt")
 
-	grid = make([][]bool, 0)
+	grid := make([][]bool, 0)
 	rolls := make([]Vec2D, 0)
 
 	for y, line := range input {
@@ -33,18 +31,52 @@ func main() {
 		grid = append(grid, gridRow)
 	}
 
+	fmt.Printf("Part 1: %v\n", part1(rolls, grid))
+	fmt.Printf("Part 2: %v\n", part2(rolls, grid))
+
+}
+
+func part1(initialRolls []Vec2D, initialGrid [][]bool) int {
 	accessibles := 0
-	for _, roll := range rolls {
-		adjacents := checkAdjacent(roll)
+	for _, roll := range initialRolls {
+		adjacents := checkAdjacent(roll, initialGrid)
 
 		if adjacents < 4 {
 			accessibles++
 		}
 	}
-	fmt.Println(accessibles)
+
+	return accessibles
 }
 
-func checkAdjacent(pos Vec2D) int {
+func part2(initialRolls []Vec2D, initialGrid [][]bool) int {
+	accessibles := 0
+
+	currentRolls := append(make([]Vec2D, 0), initialRolls...)
+	nextRolls := make([]Vec2D, 0)
+	wasChange := true
+
+	for wasChange {
+		wasChange = false
+		for _, roll := range currentRolls {
+			adjacents := checkAdjacent(roll, initialGrid)
+
+			if adjacents < 4 {
+				accessibles++
+				initialGrid[roll.Y][roll.X] = false
+				wasChange = true
+			} else {
+				nextRolls = append(nextRolls, roll)
+			}
+		}
+		currentRolls = nextRolls
+		nextRolls = make([]Vec2D, 0)
+	}
+
+	return accessibles
+}
+
+func checkAdjacent(pos Vec2D, grid [][]bool) int {
 	count := 0
 	for x := pos.X - 1; x <= pos.X+1; x++ {
 		if x < 0 || x >= len(grid[0]) {
