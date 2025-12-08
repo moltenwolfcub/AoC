ce4760b4c40ca7d70a9e501924f938dff36c5340
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-08 22:30:17 +0000

    2025-6-2

--- 2025/day6/main.go
+++ 2025/day6/main.go
@@ -33,6 +33,12 @@ type Equation struct {
 func main() {
 	input := helpers.ReadLines("input.txt")
 
+	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
+}
+
+func part1(input []string) int {
+
 	equations := make([]Equation, len(strings.Fields(input[0])))
 
 	for _, line := range input {
@@ -58,6 +64,62 @@ func main() {
 		}
 	}
 
+	ans := calcEquations(equations)
+	return ans
+}
+
+func part2(input []string) int {
+
+	equations := make([]Equation, 0)
+	rowLen := len(input[0])
+
+	eq := Equation{}
+	eq.operands = make([]int, 0)
+	for i := 0; i < rowLen; i++ {
+
+		value := 0
+		filled := false
+		for _, line := range input {
+			if line == "" {
+				continue
+			}
+
+			c := line[i]
+			if c != ' ' {
+				filled = true
+			} else {
+				continue
+			}
+
+			if c == '+' {
+				eq.operator = add
+				continue
+			}
+			if c == '*' {
+				eq.operator = mul
+				continue
+			}
+
+			d := int(c) - 48 // convert rune -> int
+			value *= 10
+			value += d
+		}
+		if filled {
+			eq.operands = append(eq.operands, value)
+		} else {
+			equations = append(equations, eq)
+
+			eq = Equation{}
+			eq.operands = make([]int, 0)
+		}
+	}
+	equations = append(equations, eq)
+
+	ans := calcEquations(equations)
+	return ans
+}
+
+func calcEquations(equations []Equation) int {
 	total := 0
 	for _, eq := range equations {
 		result := int(eq.operator)
@@ -69,5 +131,5 @@ func main() {
 		total += result
 	}
 
-	fmt.Println(total)
+	return total
 }
