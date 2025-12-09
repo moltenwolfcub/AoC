a6c9f6f40c2c628e7c2b987fde702aef43ff8c42
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-09 20:57:53 +0000

    added profiler for day8

--- 2025/day8/main.go
+++ 2025/day8/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math"
+	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
 	"sort"
 	"strings"
 
@@ -37,7 +42,23 @@ func (c Circuit) Merge(o Circuit) *Circuit {
 	return newCircuit
 }
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+
 func main() {
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	input := helpers.ReadLines("input.txt")
 
 	boxes := make([]*JunctionBox, len(input)-1)
