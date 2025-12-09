47d5edf693284646c308256ef7850b3f7fc489d5
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-09 19:41:55 +0000

    2025-8-1

---
+++ 2025/day8/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"slices"
+	"sort"
+	"strings"
+
+	"github.com/moltenwolfcub/AoC/helpers"
+)
+
+type JunctionBox struct {
+	X, Y, Z int
+	Circuit *Circuit
+}
+
+func (v JunctionBox) Dist(o JunctionBox) float64 {
+	return math.Sqrt(float64((v.X-o.X)*(v.X-o.X) + (v.Y-o.Y)*(v.Y-o.Y) + (v.Z-o.Z)*(v.Z-o.Z)))
+}
+
+type Circuit struct {
+	Boxes []*JunctionBox
+}
+
+func (c Circuit) Merge(o Circuit) *Circuit {
+	newCircuit := &Circuit{
+		Boxes: make([]*JunctionBox, len(c.Boxes)+len(o.Boxes)),
+	}
+	for i, box := range c.Boxes {
+		box.Circuit = newCircuit
+		newCircuit.Boxes[i] = box
+	}
+	for i, box := range o.Boxes {
+		box.Circuit = newCircuit
+		newCircuit.Boxes[len(c.Boxes)+i] = box
+	}
+	return newCircuit
+}
+
+func main() {
+	input := helpers.ReadLines("input.txt")
+	const maxConnection int = 1000
+
+	boxes := make([]*JunctionBox, len(input)-1)
+	circuits := make([]*Circuit, len(input)-1)
+	for i, line := range input {
+		if line == "" {
+			continue
+		}
+
+		c := &Circuit{}
+		circuits[i] = c
+
+		coords := strings.Split(line, ",")
+		box := &JunctionBox{
+			helpers.MustAtoi(coords[0]),
+			helpers.MustAtoi(coords[1]),
+			helpers.MustAtoi(coords[2]),
+			c,
+		}
+		c.Boxes = append(c.Boxes, box)
+		boxes[i] = box
+	}
+
+	for z := 0; z < maxConnection; z++ {
+		closest, empties := ShortestDist(boxes)
+
+		z += empties
+		if z >= maxConnection {
+			break
+		}
+
+		// remove previous 2 circuits
+		toRemove := -1
+		for i, c := range circuits {
+			if c == closest[0].Circuit {
+				toRemove = i
+				break
+			}
+		}
+		circuits = append(circuits[:toRemove], circuits[toRemove+1:]...)
+		toRemove = -1
+		for i, c := range circuits {
+			if c == closest[1].Circuit {
+				toRemove = i
+				break
+			}
+		}
+		circuits = append(circuits[:toRemove], circuits[toRemove+1:]...)
+
+		// merge new circuits
+		newCircuit := closest[0].Circuit.Merge(*closest[1].Circuit)
+		circuits = append(circuits, newCircuit)
+	}
+
+	sort.SliceStable(circuits, func(i, j int) bool {
+		return len(circuits[i].Boxes) > len(circuits[j].Boxes)
+	})
+
+	product := len(circuits[0].Boxes) * len(circuits[1].Boxes) * len(circuits[2].Boxes)
+
+	fmt.Println(product)
+}
+
+func ShortestDist(boxes []*JunctionBox) ([2]*JunctionBox, int) {
+	emptyTests := 0
+
+loop:
+	least := math.Inf(1)
+	leastPair := [2]*JunctionBox{}
+	for i := 0; i < len(boxes); i++ {
+		first := boxes[i]
+
+		for j := i + 1; j < len(boxes); j++ {
+			second := boxes[j]
+
+			if first.Circuit == second.Circuit && first.Circuit != nil {
+				completed := [2]*JunctionBox{first, second}
+				if slices.Contains(completedConnections, completed) {
+					continue
+				}
+			}
+
+			d := first.Dist(*second)
+			if d < least {
+				least = d
+				leastPair[0] = first
+				leastPair[1] = second
+			}
+		}
+	}
+	if leastPair[0].Circuit == leastPair[1].Circuit && leastPair[0].Circuit != nil {
+		completedConnections = append(completedConnections, leastPair)
+		emptyTests++
+		goto loop
+	}
+	completedConnections = append(completedConnections, leastPair)
+	return leastPair, emptyTests
+}
+
+var completedConnections [][2]*JunctionBox = make([][2]*JunctionBox, 0)
