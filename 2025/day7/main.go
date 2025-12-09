5df1d4f1c2e1c70bbff1e0a291338b445f995841
MoltenWolfCub <oliver@tomlins.org.uk>
2025-12-09 10:03:52 +0000

    2025-7-2

--- 2025/day7/main.go
+++ 2025/day7/main.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Beam struct {
-	Pos int
+	Pos    int
+	Weight int
 }
 
 func main() {
@@ -28,12 +29,17 @@ func main() {
 			case '^':
 				row[x] = true
 			case 'S':
-				start = Beam{x}
+				start = Beam{x, 1}
 			}
 		}
 		grid[y] = row
 	}
 
+	fmt.Printf("Part 1: %v\n", part1(grid, start))
+	fmt.Printf("Part 2: %v\n", part2(grid, start))
+}
+
+func part1(grid [][]bool, start Beam) int {
 	currentBeams := []Beam{start}
 	nextBeams := make([]Beam, 0)
 
@@ -42,8 +48,8 @@ func main() {
 		for _, beam := range currentBeams {
 			if row[beam.Pos] {
 				splits++
-				left := Beam{beam.Pos - 1}
-				right := Beam{beam.Pos + 1}
+				left := Beam{beam.Pos - 1, 1}
+				right := Beam{beam.Pos + 1, 1}
 
 				if !slices.Contains(nextBeams, left) {
 					nextBeams = append(nextBeams, left)
@@ -59,10 +65,55 @@ func main() {
 				}
 			}
 		}
-		currentBeams = make([]Beam, len(nextBeams))
+		currentBeams = make([]Beam, 0)
 		currentBeams = append(currentBeams, nextBeams...)
 		nextBeams = make([]Beam, 0)
 	}
 
-	fmt.Println(splits)
+	return splits
+}
+
+func part2(grid [][]bool, start Beam) int {
+	currentBeams := []Beam{start}
+	nextBeams := make([]Beam, 0)
+
+	for _, row := range grid {
+		for _, beam := range currentBeams {
+			if row[beam.Pos] {
+				left := Beam{beam.Pos - 1, beam.Weight}
+				right := Beam{beam.Pos + 1, beam.Weight}
+
+				l := slices.Index(nextBeams, left)
+				if l < 0 {
+					nextBeams = append(nextBeams, left)
+				} else {
+					nextBeams[l].Weight += left.Weight
+				}
+
+				r := slices.Index(nextBeams, right)
+				if r < 0 {
+					nextBeams = append(nextBeams, right)
+				} else {
+					nextBeams[r].Weight += right.Weight
+				}
+			} else {
+				b := slices.Index(nextBeams, beam)
+				if b < 0 {
+					nextBeams = append(nextBeams, beam)
+				} else {
+					nextBeams[b].Weight += beam.Weight
+				}
+			}
+		}
+		currentBeams = make([]Beam, 0)
+		currentBeams = append(currentBeams, nextBeams...)
+		nextBeams = make([]Beam, 0)
+	}
+
+	universes := 0
+	for _, b := range currentBeams {
+		universes += b.Weight
+	}
+
+	return universes
 }
